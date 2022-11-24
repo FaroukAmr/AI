7f0172d3d199625a5065a3d2416c7824f7604cd8
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-11-24 15:04:03 +0200

    initial

---
+++ src/CoastGuard.java
@@ -0,0 +1,10 @@
+public class CoastGuard {
+
+    public static String GenGrid(){
+        return "";
+    }
+
+    public static String Solve(String grid,String strategy,boolean visualize){
+        return "";
+    }
+}
--- src/SearchProblem.java
+++ src/SearchProblem.java
@@ -1,5 +1,4 @@
 public class SearchProblem {
     public static void main(String[] args){
-    System.out.println("");
     }
 }
